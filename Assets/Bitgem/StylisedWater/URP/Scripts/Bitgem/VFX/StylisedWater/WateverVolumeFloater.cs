eb5fbc970fda0b553079d985c6ad3558da321052
kremfote <flyingnorwegians@gmail.com>
2026-09-18 17:47:23 +0200

    fixed update conflicts

---
+++ Assets/Bitgem/StylisedWater/URP/Scripts/Bitgem/VFX/StylisedWater/WateverVolumeFloater.cs
@@ -0,0 +1,36 @@
+﻿#region Using statements
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#endregion
+
+namespace Bitgem.VFX.StylisedWater
+{
+    public class WateverVolumeFloater : MonoBehaviour
+    {
+        #region Public fields
+
+        public WaterVolumeHelper WaterVolumeHelper = null;
+
+        public float yOffset = 1.0f;
+
+        #endregion
+
+        #region MonoBehaviour events
+
+        void Update()
+        {
+            var instance = WaterVolumeHelper ? WaterVolumeHelper : WaterVolumeHelper.Instance;
+            if (!instance)
+            {
+                return;
+            }
+            
+            transform.position = new Vector3(transform.position.x, instance.GetHeight(transform.position) + yOffset ?? transform.position.y + yOffset, transform.position.z);
+        }
+
+        #endregion
+    }
+}
